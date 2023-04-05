d27792c79fabbb081dc1fc303a7cc8e30ab2e585
Maria Esmeralda <6580@holbertonstudents.com>
2023-04-05 04:07:25 -0700

    added c file to print single digit numbers from 0 to base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description - prints single digit numbers from 0 to base 10
+ *
+ * return: 0
+ */
+int main(void)
+{
+	puts("0123456789");
+	return (0);
+}
