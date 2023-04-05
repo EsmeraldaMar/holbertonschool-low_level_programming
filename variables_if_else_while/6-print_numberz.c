b5687194d5d53a0aab4c4fbc6f6c395033bebd27
Maria Esmeralda <6580@holbertonstudents.com>
2023-04-05 04:33:25 -0700

    added c file to print single numbers using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * description - prints single digit numbers from 0 to base 10 using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
