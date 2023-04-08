99db981b94b42a0025e9e1cfe60b1e694355a715
Maria Esmeralda <6580@holbertonstudents.com>
2023-04-08 06:33:18 -0700

    added file to add two integers together

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - it adds two integers together
+ * @a: the first integer to be added.
+ * @b: the second integer to be added to a.
+ * description: it adds two integers together and prints out the result
+ *
+ * Return: result of added integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
