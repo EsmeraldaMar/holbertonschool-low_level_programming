1760076a65b674eb5663f5cda667db705ef6cbd3
Maria Esmeralda <6580@holbertonstudents.com>
2023-04-22 21:17:08 -0700

    Added task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * puts_half - it prints the second half of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n;
+	int i;
+
+	n = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	
+	}
+	while (str[n] != '\0')
+	{
+		if (n > ((i - 1) / 2))
+		{
+			_putchar(str[n]);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
