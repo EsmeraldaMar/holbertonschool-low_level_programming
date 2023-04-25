dd417ec46b3801d5a25995ab7e7797b4b49b08f5
Maria Esmeralda <6580@holbertonstudents.com>
2023-04-24 23:38:47 -0700

    added file for task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - it copies a string from one destination to another
+ * @dest: destination, string is to be copied in
+ * @src: string to be copied
+ * @n: number of characters to be copied
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
