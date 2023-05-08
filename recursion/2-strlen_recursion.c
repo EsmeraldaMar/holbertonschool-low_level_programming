2848e226f6e1565e0cd87dbe8fe5322ba81ff050
Maria Esmeralda <6580@holbertonstudents.com>
2023-05-08 04:36:48 -0700

    addef file for task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion -it returns a length of a string.
+ * @s: string to be checked
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
