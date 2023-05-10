c928ab3a3ba7c734b14d6f9c05192707fbb1591f
Maria Esmeralda <6580@holbertonstudents.com>
2023-05-09 23:42:46 -0700

    fix 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -27,9 +27,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
+	if (n == 0 || n == 1)
+		return (n);
 	return (_sqrt_recursion_loop(n, 1));
 }
