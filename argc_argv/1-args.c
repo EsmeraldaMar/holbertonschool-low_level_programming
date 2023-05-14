29cea26b1b17ec650f475723cdd2c60bfeb73cf3
Maria Esmeralda <6580@holbertonstudents.com>
2023-05-14 02:36:51 -0700

    added task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - it prints a string then a new line to stdout
+ * @str: is pointer to variable to print string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar (str[i]);
+		i++;
+	}
+	_putchar ('\n');
+}
+
+/**
+ * main - Entry Point
+ *
+ * @argc: it counts number of parameters which go to main
+ * @argv: pointer to array of pointers which has strings entering main
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	_putchar(argc - 1 + 48);
+	_putchar('\n');
+	(void) argv;
+	return (0);
+}
