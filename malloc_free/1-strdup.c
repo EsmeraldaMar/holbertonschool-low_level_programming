cb4a4fc38ca3d3f2b02dbb1ffba9ee28fb223585
Maria Esmeralda <6580@holbertonstudents.com>
2023-05-25 01:26:34 -0700

    added file for task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
 /**
- * _strdup: it returns pointr to new string where string is copied
+ * _strdup - it returns pointr to new string where string is copied
  * @str: string to duplicate and to point to
  * Return: NULL if string is NULL or if not enough space available,
  * else pointer to copied string
@@ -10,11 +10,26 @@ char *_strdup(char *str)
 {
 	char *newstr;
 	int size = 0;
-	int i;
+	int i = 0;
 
 	if (str == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	for (i = 0
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	newstr = malloc(sizeof(char) * (size + 1));
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
 }
