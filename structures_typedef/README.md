7aba1c948c03ef87d03dd57f4ba000d81798a310
Maria Esmeralda <6580@holbertonstudents.com>
2023-06-07 03:31:26 -0700

    added file 0 for doggo

---
+++ structures_typedef/README.md
@@ -0,0 +1,7 @@
+Structures TypeDef
+0) create struct dog with specified elements.
+1) it initializes variable of type struct dog.
+2) it prints struct dog
+3)it defines new type dog_t as new name for struct dog.
+4) it creates a new dog and stores copy of name and owner.
+5) it frees dogs.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - new type of data for doggo
+ * @name: name of doggo
+ * @age: age of doggo, is he young or old?
+ * @owner: doggo's owner's name, food giver
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
