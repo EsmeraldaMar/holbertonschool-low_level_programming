f96e9fd4e47aef3fe14643497b282e887560e5d6
Maria Esmeralda <6580@holbertonstudents.com>
2023-06-14 04:31:07 -0700

    added file for task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - it executes function for eacc parameter in array
+ * @array: array of int
+ * @size: size of array
+ * @action: pointer to function used
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i = 0;
+
+	while (i < size)
+	{
+		(*action)(array[i]);
+		i++;
+	}
+}
