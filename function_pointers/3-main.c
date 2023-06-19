be3203751cbb79a79795544313387a927829fc85
Maria Esmeralda <6580@holbertonstudents.com>
2023-06-18 21:09:27 -0700

    added file for task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,6 +22,11 @@ int main(int argc, char *argv[])
 	first = atoi(argv[1]);
 	second = atoi(argv[3]);
 	op = argv[2];
+	if (strlen(op) != 1)
+	{
+		puts("Error");
+		exit(99);
+	}
 	if (get_op_func(op) == NULL)
 	{
 		puts("Error");
