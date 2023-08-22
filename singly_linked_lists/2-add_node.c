93edbf4729c395dfc8f17838eb82c1b24511ad39
Maria Esmeralda <6580@holbertonstudents.com>
2023-08-22 03:33:54 -0700

    added file for task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "list.h"
+
+/**
+ * add_node - adds a node to the beginning of the list
+ * @head: beginning of the list
+ * @str: string to add
+ *
+ * Return: addredd of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *element;
+	unsigned int i = 0;
+
+	element = malloc(sizeof(list_t));
+	if (element != NULL)
+	{
+		while (str[i] != \0)
+			i++;
+		element->str = strdup(str);
+		element->len = i;
+		element->next = *head;
+	}
+	else
+		return (NULL);
+	if (*head != NULL)
+		element->next = *head;
+	*head = element;
+	return (element);
+}
