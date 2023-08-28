1465a09ba788de151f4b702382b57fec2d714a43
Maria Esmeralda <6580@holbertonstudents.com>
2023-08-28 04:44:44 -0700

    added file for task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: binary
+ *
+ * Return: converted binary
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	
+	unsigned int num = 0;
+
+	if (b)
+	{
+		while (*b)
+		{
+			if (*b == '1')
+				num = (num << 1) | 1;
+			else if (*b == '0')
+				num <<= 1;
+			else
+				return (0);
+			b += 1;
+		}
+	}
+	return (num);
+}
