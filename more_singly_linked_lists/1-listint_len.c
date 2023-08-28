b1c3319a7e3a3c805d8352f82fc86f5fe0535789
Maria Esmeralda <6580@holbertonstudents.com>
2023-08-27 20:17:13 -0700

    added file for task 1

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listont_len - counts the number of element
+ * @h: head of list
+ *
+ * Return: number of element
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *counter = h;
+	size_t count = 0;
+
+	while (counter != NULL)
+	{
+		count += 1;
+		counter = counter->next;
+	}
+	return (count);
+}
+
