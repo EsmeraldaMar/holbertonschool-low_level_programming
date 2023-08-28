b5bf6a2d2fcd69e56401ca50ea4c8d7a1c878d66
Maria Esmeralda <6580@holbertonstudents.com>
2023-08-28 05:06:09 -0700

    fix

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -5,12 +5,12 @@
  * @n: number
  * @index: index
  *
- * Return: ! or -1
+ * Return: 1 or -1
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
+	if (n == NULL || index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	*n |= (1 << index);
-	return (0);
+	return (1);
 }
