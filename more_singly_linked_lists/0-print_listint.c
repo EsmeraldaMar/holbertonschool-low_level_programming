1698759c171493c075c13c5e81ff6191d2d58ab2
Maria Esmeralda <6580@holbertonstudents.com>
2023-08-27 20:12:19 -0700

    added file for task 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements
+ * @h: head of list
+ *
+ * Return: number of element
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *counter = h;
+	size_t count = 0;
+
+	while (counter != NULL)
+	{
+		printf("%d\n", counter->n);
+		count += 1;
+		counter = counter->next;
+	}
+	return (count);
+}
