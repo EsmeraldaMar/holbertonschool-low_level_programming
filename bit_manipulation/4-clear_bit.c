310541c0a09874ee2d64a289e4fa85e17ab605cf
Maria Esmeralda <6580@holbertonstudents.com>
2023-08-28 04:58:56 -0700

    added file for task 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear a bit at position
+ * @number: number
+ * @index: index
+ *
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
